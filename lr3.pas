27bb5ccf141bae2b8c4834373615ff0d8019b977
zhigulin_na <zhigulin_na@taximaxim.ru>
2014-10-10 14:13:28 +0600

    ask filenames

--- lr3.pas
+++ lr3.pas
@@ -4,8 +4,11 @@ var
 	// fn : array[0..49] of integer = (1, 3, 95, 51, 14, 9, 76, 85, 53, 61, 44, 39, 63, 31, 84, 28, 94, 2, 63, 27, 96, 7, 79, 20, 20, 37, 14, 69, 83, 21, 34, 39, 68, 72, 75, 72, 82, 66, 25, 55, 84, 26, 98, 52, 47, 95, 92, 48, 82, 31);
 	fn : array[0..49] of integer;
 	number, temp, i, j, j1, final : integer;
+	fName, gName : string;
 begin
-	Assign (f, '/home/zhigulin_na/Sandbox/playground/tusur/algo/kr2/lr3/f.txt');
+	writeln('Source file name:');
+	readln(fName);
+	Assign (f, fName);
 	// Rewrite(f);
 	// for i := 0 to 49 do
 	// Write(f, fn[i]);
@@ -19,7 +22,7 @@ begin
 			if(number mod 2>0) then 
 			begin
 				Inc(i);
-				fn[i]:=number;
+				fn[i] := number;
 			end;
 		end;
 	for j1 := i DownTo 0 do
@@ -30,9 +33,12 @@ begin
 				fn[j-1] := fn[j];
 				fn[j] := temp;
 			end;
-	Assign (g, '/home/zhigulin_na/Sandbox/playground/tusur/algo/kr2/lr3/g.txt');
+	writeln('Output file name:');
+	readln(gName);
+	Assign (g, gName);
 	Rewrite(g);
 	for final := 0 to i do 
+		// writeln(fn[final]);
 		write(g, fn[final]);
 
 	Close(f);
