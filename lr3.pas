01b7aa143c1fa80e679a870dda11987faa78b02a
zhigulin_na <zhigulin_na@taximaxim.ru>
2014-10-13 11:57:43 +0600

    fix runtime error, избавились от дубликатов и нулевых значений

--- lr3.pas
+++ lr3.pas
@@ -1,14 +1,16 @@
 Program lr3;
 var
 	f, g : file of integer;
-	fn : array[0..49] of integer;
+	fn, finalArray : array[0..49] of integer;
 	number, temp, i, j, j1, final : integer;
 	fName, gName : string;
+	duplicate : Boolean;
 begin
 	{Получаем путь к файлу}
 	writeln('Source file name:');
 	readln(fName);
 	Assign (f, fName);
+	reset(f);
 	i := -1;
 	while not eof(f) do 
 		begin
@@ -29,6 +31,24 @@ begin
 				fn[j-1] := fn[j];
 				fn[j] := temp;
 			end;
+	{Удаляем дубликаты}
+	finalArray[1] := fn[1];
+	j := 1;
+	temp := 1;
+	while j < i do
+	begin
+		Inc(j);
+		duplicate := false;
+		for j1 := 1 to temp do
+			if fn[j] = finalArray[j1] then begin
+				duplicate := true;
+				break;
+			end;
+			if duplicate=false then begin
+				inc(temp);
+				finalArray[temp] := fn[j];
+			end;
+	end;
 	{Получаем путь к файлу в который нужно записать результат}
 	writeln('Output file name:');
 	readln(gName);
@@ -36,8 +56,10 @@ begin
 	Rewrite(g);
 	{Записываем результат, параллельно выводим его в консоль}
 	for final := 0 to i do begin
-		write(g, fn[final]);
-		write(fn[final]);
+		if finalArray[final] <> 0 then begin
+			write(g, finalArray[final]);
+			write(finalArray[final], ' ');
+		end;
 	end;
 	{Закрываем оба файла}
 	Close(f);
